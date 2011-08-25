f19660ded2c728e3a7de40274f816f7ae63c84e3
David Piegza <piegza@workhub.com>
2011-08-25 19:26:10 +0200

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,6 @@
+Graphit
+===================
+
+Graphit is an Ruby on Rails web application for 3D graph drawing with WebGL.
+
+It uses [Graph Visualization](git@github.com:davidpiegza/Graph-Visualization.git) for graph drawing.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graphit::Application.config.secret_token = '8d37451040780d4cfacdb25b49b73674bac4935d7d3039f49045ccb58383ace57a2a3ad3b012373dd2c7acda12418235944717edfdd8641a8ba497acf73c1070'
